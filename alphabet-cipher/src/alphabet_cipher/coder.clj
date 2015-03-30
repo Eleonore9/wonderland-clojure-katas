6c22ca71fd7ad9c895fff65759ee76a402bb478a
Eleonore9 <eleonore.mayola@ymail.com>
2015-03-30 18:35:37 +0100

    Built the substitution chart! ^^

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,26 +1,42 @@
-(ns alphabet-cipher.coder)
-
-(defn encode [keyword message]
-  "encodeme")
-
-(defn decode [keyword message]
-  "decodeme")
+(ns alphabet-cipher.coder
+  (:require [clojure.string :as s]))
 
 ;;I want to build something like that:
-;; {:A {:A "a" :B "b" :C "c" :D "d" :E "e" :F "f" :G "g" ...}
-;;  :B {:A "b" :B "c" :C "d" :D "e" :E "f" :F "g" :G "h" ...}}
+;; {"A" {"A" "a" "B" "b" "C" "c" "D" "d" "E" "e" "F "f" "G" "g" ...}
+;;  "B" {"A" "b" "B" "c" "C" "d" "D" "e" "E" "f" "F" "g" "G" "h" ...}}
 
-;; for now:
+;; Sequences of the alphabet:
 (def up-alphabet (mapv str (map char (concat (range 65 91)))))
 ;; ["A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z"]
+
 (def lo-alphabet (mapv str (map char (concat (range 97 123)))))
 ;; ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"]
 
-(reduce conj (mapv (fn [x] {(keyword (str x)) 
-                            (clojure.string/lower-case x)}) up-alphabet))
-{:L "l", :M "m", :I "i", :R "r", :O "o", :A "a", :F "f", :W "w", :Q "q", :P "p", :D "d", :B "b", :J "j", :Z "z", :T "t", :C "c", :E "e", :G "g", :Y "y", :X "x", :H "h", :V "v", :U "u", :S "s", :N "n", :K "k"}
+(defn sort-by-letter
+  "Sorts a vector of letters as strings 
+  starting with the letter passed in."
+  [letters-vec letter]
+  (into [] (concat (subvec letters-vec
+                           (.indexOf letters-vec letter))
+                   (subvec letters-vec 0
+                           (.indexOf letters-vec letter)))))
+
+(defn substitution-chart
+  "Builds the substitution map
+  used to code and decode."
+  []
+  (map #(hash-map %
+                  (zipmap
+                   up-alphabet
+                   (sort-by-letter
+                    lo-alphabet
+                    (s/lower-case %))))
+       up-alphabet))
 
 
-(def decoder
-  (let [up-alphabet (map char (concat (range 65 91)))
-        lo-alphabet (map char (concat (range 97 123)))]))
+(defn encode [keyword message]
+  "encodeme"
+  )
+
+(defn decode [keyword message]
+  "decodeme")
